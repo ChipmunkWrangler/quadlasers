5c8d9db30e12e5a0c8e537ac1394f20cf8a47f5d
Rafael <rafael@Rafaels-MacBook-Air.local>
2018-02-09 16:42:23 +0100

    Added "tap to continue" text

--- Assets/Scripts/GameOverOnContact.cs
+++ Assets/Scripts/GameOverOnContact.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class GameOverOnContact : MonoBehaviour {
 	[SerializeField] UnityEngine.UI.Text gameOverText;
+	[SerializeField] UnityEngine.UI.Text tapToContinueText;
 
 	void Start () {
 		gameOverText.CrossFadeAlpha( 0.0f, 0.0f, false );
+		tapToContinueText.CrossFadeAlpha( 0.0f, 0.0f, false );
 	}
 
 	void OnTriggerEnter () {
@@ -16,6 +18,9 @@ public class GameOverOnContact : MonoBehaviour {
 	void GameOver () {
 		if (gameOverText != null) {
 			gameOverText.CrossFadeAlpha( 1.0f, 2.0f, true );
+		}
+		if (tapToContinueText != null) {
+			tapToContinueText.CrossFadeAlpha( 1.0f, 5.0f, true );
 		}
 		Time.timeScale = 0;
 		Debug.Log( "Game over" );
