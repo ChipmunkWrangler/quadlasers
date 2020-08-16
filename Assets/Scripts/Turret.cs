66d6cf98e373de630befb1deb687f64261087d9c
Rafael <consulting@crazy-chipmunk.com>
2020-08-16 09:53:03 +0200

    Use attitude instead of compass

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,82 +1,40 @@
-﻿#if UNITY_IOS || UNITY_ANDROID
-#define COMPASS_ROTATION
+﻿#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+#define ATTITUDE_BASED_ROTATION
 #endif
-
 using UnityEngine;
-using System.Collections.Generic;
-using UnityEngine.UI;
 
 
 public class Turret : MonoBehaviour {
-	//	public GameObject shot;
-	//	public Transform shotSpawn;
-	// COMPASS_ROTATION
-	public float MAX_ACCEPTABLE_HEADING_INACCURACY;
-	public GameObject[] subscribers;
-	public Text debugText_accuracy;
-	public Text debugText_heading;
-	// MOUSE ROTATION
-	public float speed;
-	#if COMPASS_ROTATION
-	private bool wasCalibrated;
-	
-
-
-#else
+	public float mouseRotationSpeed;
+#if !ATTITUDE_BASED_ROTATION
 	private Rigidbody rb;
-	#endif
+#endif
 
-	void Start () {
-		#if COMPASS_ROTATION
-		Input.compass.enabled = true;
-		wasCalibrated = true;
-		print( "Compass" );
-		#else
-		rb = GetComponent<Rigidbody>();
-		#endif
-	}
-
-	void FixedUpdate () {
-#if COMPASS_ROTATION
-		transform.rotation = GetCompassRotation();
+	private void Start () {
+#if ATTITUDE_BASED_ROTATION
+		Input.gyro.enabled = true;
 #else
-		Vector3 rotationVelocity = GetMouseInput();
-//		rb.AddTorque( transform.right * rotationVelocity.x );	
-		rb.AddTorque( transform.up * rotationVelocity.y );
+		rb = GetComponent<Rigidbody>();
 #endif
 	}
 
-	#if COMPASS_ROTATION
-	Quaternion GetCompassRotation () {
-		debugText_accuracy.text = Input.compass.headingAccuracy.ToString();
-		debugText_heading.text = Input.compass.magneticHeading.ToString();
-		if (IsCompassCalibrated()) {
-			SetCalibratedStatus( true );
-			return Quaternion.Euler( 0, Input.compass.magneticHeading, 0 );
-		} else {
-			SetCalibratedStatus( false );
-			return transform.rotation;
-		}
+#if ATTITUDE_BASED_ROTATION
+	private void Update()
+	{
+			transform.rotation = Input.gyro.attitude;
+			transform.Rotate( 0f, 0f, 180f, Space.Self ); // Swap "handedness" of quaternion from gyro.
+			transform.Rotate( 90f, 180f, 0f, Space.World ); // Rotate to make sense as a camera pointing out the back of your device.
 	}
-
-	bool IsCompassCalibrated () {
-		return Input.compass.headingAccuracy >= 0 && Input.compass.headingAccuracy < MAX_ACCEPTABLE_HEADING_INACCURACY;
-	}
-
-	void SetCalibratedStatus (bool isCalibrated) {
-		if (isCalibrated != wasCalibrated) {
-			AssemblyCSharp.SharedLibrary.InformSubscribers( subscribers, "CompassCalibrated", isCalibrated );
-			wasCalibrated = isCalibrated;
-		}
-
-	}
-
-	
-
-
 #else
-	Vector3 GetMouseInput () {
-		return new Vector3( -Input.GetAxis( "Mouse Y" ), Input.GetAxis( "Mouse X" ), 0.0f ) * speed;
+	private void FixedUpdate ()
+	{
+		Vector3 rotationVelocity = GetMouseInput();
+		rb.AddTorque( transform.right * rotationVelocity.x );	
+		rb.AddTorque( transform.up * rotationVelocity.y );
 	}
-	#endif
+	
+	Vector3 GetMouseInput () {
+		return new Vector3( -Input.GetAxis( "Mouse Y" ), Input.GetAxis( "Mouse X" ), 0.0f ) * mouseRotationSpeed;
+	}
+#endif
 }
